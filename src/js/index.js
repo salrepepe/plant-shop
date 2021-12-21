166dfe48c75b9ce45ec7dbd5eabc4eca4ceda76b
salrepepe <muhadeputat@gmail.com>
2021-12-22 04:10:00 +0600

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".header");
+  header.classList.toggle("header_sticky", window.scrollY > 0);
+});
+
+const nav = document.querySelector(".nav");
+const burger = document.querySelector(".burger");
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  nav.classList.toggle("nav_active");
+});
